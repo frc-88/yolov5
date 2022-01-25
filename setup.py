bf4a11a715baae34b72a32e290ee6733620531d3
Woz4tetra <woz4tetra@gmail.com>
2022-01-25 01:13:11 -0500

    reorganizing to work in module format

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yolov5',
+    version='0.0',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    # run installation requirements file
+)
